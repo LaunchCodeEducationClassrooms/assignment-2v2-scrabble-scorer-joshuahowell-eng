f76d3ed1b513d6f771065b194af6b5c07ad4f9a8
joshuathowell <joshua.t.howell@gmail.com>
2021-03-04 23:16:14 +0000

    removed commas in object

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -74,17 +74,17 @@ const scoringAlgorithms = [
   {
     name: 'Simple Score',
     description: 'Each letter is worth 1 point.',
-    scorerFunction: simpleScore,
+    scorerFunction: simpleScore
   },
   {
     name: 'Bonus Vowels',
     description: 'Vowels are 3 pts, consonants are 1 pt.',
-    scorerFunction: vowelBonusScore,
+    scorerFunction: vowelBonusScore
   },
   {
     name: 'Scrabble',
     description: 'The traditional scoring algorithm.',
-    scorerFunction: scrabbleScore,
+    scorerFunction: scrabbleScore
   }
 ];
 
